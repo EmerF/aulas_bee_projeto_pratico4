f9cb8772d539ff6b19d5a22f7cf725c1612969c2
Leticia Affonso <le.affonsosoares.gmail.com>
2023-11-17 18:40:16 -0300

    base

---
+++ src/main/java/models/Refrigerante.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Refrigerante extends Produtos {
+    private boolean comAcucar;
+
+    public boolean isComAcucar() {
+        return comAcucar;
+    }
+
+    public void setComAcucar(boolean comAcucar) {
+        this.comAcucar = comAcucar;
+    }
+}
+
