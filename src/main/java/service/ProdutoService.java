a92f31dd3c4d80077b59d7433d1a0c9d1ba55c61
andressaevelyn <contatoaevelyn@gmail.com>
2023-11-18 19:41:27 -0300

    Projeto pratico 4 - java 2

--- src/main/java/service/ProdutoService.java
+++ src/main/java/service/ProdutoService.java
@@ -1,4 +1,30 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarPorId(String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public Produto buscarPorNome(String nome) {
+        return produtoRepository.findByNome(nome);
+    }
+
+    public List<Produto> buscarTodos() {
+        return produtoRepository.findAll();
+    }
 }
+
